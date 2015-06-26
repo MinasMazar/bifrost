4e6241d16d56bc95dbc3dd6806b3a8a3047a8350
MinasMazar <minasmazar@gmail.com>
2015-06-26 02:28:50 +0200

    - Middleman article command also generate data directory for article - Refact #system_cmds -> #system_exec, #system-exec_with_output - Include Logger and Pry for debug

--- lib/bifrost.rb
+++ lib/bifrost.rb
@@ -1,5 +1,32 @@
 require "bifrost/version"
+require "logger"
+
+$logger = Logger.new STDOUT
 
 module Bifrost
-  # Your code goes here...
+
+  def system_exec(cmds)
+    cmds = [ cmds ] if cmds.is_a? String
+    cmds.map do |cmd|
+      $logger.debug "Executing system command: #{cmd}"
+      system "#{cmd}"
+    end
+  end
+
+  def system_exec_and_get_output(cmds)
+    cmds = [ cmds ] if cmds.is_a? String
+    cmds.map do |cmd|
+      $logger.debug "Executing system command: #{cmd}"
+      `#{cmd}`
+    end
+  end
+
+  def select_executable(cmds)
+    cmds.find { |c| check_executable c }
+  end
+
+  def check_executable(cmd)
+    system(cmd + ">/dev/null 2>/dev/null") != nil ? cmd : false
+  end
+
 end
