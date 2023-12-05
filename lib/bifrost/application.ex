7607ca94609dd15faa9f442a6cbfe6a8049efabd
Invisigoth <invisigoth@MacBook-Pro-di-Invisigoth.local>
2023-12-05 16:05:27 +0100

    Initial commit
    $ mix new bifrost --sup

---
+++ lib/bifrost/application.ex
@@ -0,0 +1,20 @@
+defmodule Bifrost.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Bifrost.Worker.start_link(arg)
+      # {Bifrost.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Bifrost.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
